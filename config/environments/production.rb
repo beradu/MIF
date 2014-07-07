36bf9df6fd093284eb1f2f4ee247d365e391066c
Brad Rossman <brad5131@gmail.com>
2014-07-06 22:01:58 -0400

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ MIF::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+   # Required for Devise
+   # Get Heroku website
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 MIF::Application.routes.draw do
+  devise_for :users
   resources :cards
 
-  root 'pages#index'
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
